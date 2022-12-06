9c8a0b7dbc50963663673356552032657c114f8e
Raphael Francisco Alves <raphaelbatealves@gmail.com>
2022-12-05 21:23:24 -0300

    Initial navigation bar and footer

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("menuMobile").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("menuMobile").style.width = "0";
+  }
